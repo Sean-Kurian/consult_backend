2998967a457a7608024a1434dc647dd3726f6ee8
Daniel Kwan <danielkwan@daniels-mbp.home>
2024-01-06 23:01:53 -0500

    change names of controller and route

--- src/routes/sowRoute.js
+++ src/routes/sowRoute.js
@@ -1,12 +1,12 @@
 // route.js
 import express from "express";
 import testController from "../controllers/controller.js";
-import sowController from "../controllers/sow.js";
+import sowController from "../controllers/sowController.js";
 
 const testRouter = express.Router();
 
 // Define your routes
-testRouter.get("/number", testController);
+
 
 testRouter.get("/sow", sowController);
 
