bc8c7a7d87721b4c4facd6dce5f6cd15db8a567a
Daniel Kwan <danielkwan@daniels-mbp.home>
2024-01-09 21:48:21 -0500

    adding external document chunking and similarity search on req.body.keys

--- src/utils/llm.js
+++ src/utils/llm.js
@@ -1,5 +1,4 @@
 import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
-import { HNSWLib } from "@langchain/community/vectorstores/hnswlib";
 import { formatDocumentsAsString } from "langchain/util/document";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { PromptTemplate, FewShotPromptTemplate } from "@langchain/core/prompts";
@@ -25,15 +24,10 @@ import { config } from "dotenv";
 config();
 
 //milvus vectordb
+//TODO: Wrap in try catch and error handling
 
 const token = process.env.ZILLIZ_TOKEN;
 const uri = process.env.ZILLIZ_URI;
-
-// create milvus vector db client
-// const testclient = new MilvusClient({
-//   address: uri,
-//   token: token,
-// });
 
 //Will need this later
 //similar to .sleep in python,
@@ -52,12 +46,19 @@ if (loadThisDir.includes("C:\\C:\\")) {
 }
 
 console.log(loadThisDir);
+
 // const milvusClient = new Milvus();
 if (loadThisDir.includes("C:\\C:\\")) {
   loadThisDir = loadThisDir.replace("C:\\", "");
 }
+
 async function callModelforResult(filledTemplate) {
+  const model = new ChatOpenAI({ modelName: "gpt-4-1106-preview" });
   // Create docs with a loader
+  //------------------------------------------------
+  //1. LOAD PRE-EXISTING SOWS INTO VECTOR DATABASE
+  //------------------------------------------------
+
   const loader = new DirectoryLoader(loadThisDir, {
     ".json": (path) => new JSONLoader(path, "/texts"),
     ".jsonl": (path) => new JSONLinesLoader(path, "/html"),
@@ -68,22 +69,29 @@ async function callModelforResult(filledTemplate) {
   var docs = await loader.load();
   // console.log(docs);
 
-  // Load the docs into the vector store
-  //note: HNSW is in memory vector store, we can try cloud vector store to store more files zilliz or pinecone etc
-  // const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
-
+  //TODO: TRY HUGGING FACE EMBEDDING FOR COST
   //loading docs into milvus store try using different embedding
   //splitting the text into
-  // const text_splitter = new RecursiveCharacterTextSplitter();
-  // text_splitter.chunkSize = 1000;
-  // text_splitter.chunkOverlap = 200;
-  // docs = await text_splitter.splitDocuments(docs);
+  //chunking documents to gather better relevant information
+  const text_splitter = new RecursiveCharacterTextSplitter({
+    chumkSize: 300,
+    chunkOverlap: 200,
+    // seperator: "**<HEADER>**",
+  });
+
+  docs = await text_splitter.splitDocuments(docs);
 
   //TODO: currently this adds 2x documents to db everytime we run,
   // we need to change EXISTS to true and have a milvus client instead of .fromDocuments
   //currently not working for true
+  // create milvus vector db client
   const EXISTS = false;
   var milStore = null;
+  var milClient = new MilvusClient({
+    address: uri,
+    token: token,
+  });
+  //uploading files from documents to vector
   if (EXISTS == false) {
     milStore = await Milvus.fromDocuments(docs, new OpenAIEmbeddings(), {
       collectionName: "sowDataCollection",
@@ -92,44 +100,67 @@ async function callModelforResult(filledTemplate) {
         token: token,
       },
     });
-  } else {
-    milStore = new MilvusClient({
-      address: uri,
-      token: token,
-    });
   }
 
-  const model = new ChatOpenAI({ modelName: "gpt-4" });
+  //
+
+  //   else {
+  //     milStore = new MilvusClient({
+  //       address: uri,
+  //       token: token,
+  //     });
+  //   }
+
+  //------------------------------------------------
+  //2.PERFORM SIMILARITY SEARCH FOR REQUEST BODY KEYS
+  //------------------------------------------------
+
+  //TODO: this needs to be variable/parameter maybe we can decide what similarity word to search on
+  //TODO: REPLACE myObj with req.body.keys object, add to this function as parameter
+  //given all keys
+  //find the most relevant doc to section x
+  //ideally its chunked perfectly for just section
+  const myObj = [
+    "organization",
+    "client",
+    "confidentiality",
+    "projectScope",
+    "projectSolution",
+    "payment",
+    "contacts",
+  ];
+  //for each key search for that section in db and pass into chat history
+  var topxDocument = [];
+  const performSimilaritySearch = async (dataToSearchOn, limit) => {
+    try {
+      const prevTop1Doc = await milStore.similaritySearch(
+        dataToSearchOn,
+        limit
+      );
+      //   console.log(prevTop1Doc);
+      topxDocument.push({ [dataToSearchOn]: prevTop1Doc[0] });
+    } catch (error) {
+      console.error(
+        `Error searching for section "${dataToSearchOn}":`,
+        error.message
+      );
+    }
+  };
 
   //perform similarity search on external db
-  //TODO: this needs to be variable/parameter maybe we can decide what similarity word to search on
-  //getting the top1 most similar document to project solution which will be req.body.projectSolution
-  const prevTop1Doc = await milStore.similaritySearch(
-    "data science solution", //<req.body.projectSolution>
-    2
-  );
 
-  //TODO:Chunk it into gpt chain it, first step, second etc, increased context window
+  const searchPromises = [];
+  for (const section of myObj) {
+    searchPromises.push(performSimilaritySearch(section, 1));
+  }
+  //damn these promises
+  // Wait for all similarity searches to complete
+  await Promise.all(searchPromises);
+  //   console.log(topxDocument);
 
-  // console.log(prevTop1Doc);
-  // console.log(typeof prevTop1Doc);
-  // console.log("----END OF TOP DOCS-----");
-  // const prompt = PromptTemplate.fromTemplate({
-  //   conversation: ConversationChain(
-  //     prompt : `You are a helpful assistant. Help the user write a Statement of Work for a consulting project. Make sure headers are wrapped in "**"
-  //         My organization is IBM.
-  //         My client is MS
-  //         The confidentiality agreements is/are v big big confidentiality stuff.
-  //         The project scope for this project is v big project scope
-  //         The payment terms are a lot of money.
-  //         The projectSolution is v small project solution
-  //         The Food provided is mangos and fruits  `
-  //   ),
-  //   llm: model,
-  //   memory: new BufferMemory(),
-  // });
-  // const temp = `Here are statements of work   `;
-  // const prompt = PromptTemplate.fromTemplate(temp);
+  //------------------------------------------------
+  //3.ADD TO BUFFER HISTORY AND PERFORM LLM CALL
+  //------------------------------------------------
 
   const memory = new BufferMemory({ memoryKey: "history" });
   // This chain is preconfigured with a default prompt
@@ -169,13 +200,15 @@ AI:`);
   // });
 
   //LOADING ALL SIMILAR DOCS INTO MEMORY
-  prevTop1Doc.forEach(async (doc) => {
-    //   console.log(doc.pageContent);
-    const res1 = await chain.call({
-      input: ` ${doc.pageContent}`,
-    });
-    // console.log({ res1 });
-    // console.log("NEXT DOC");
+  //-----------------------------------
+  topxDocument.forEach(async (doc) => {
+    //   console.log(doc[0]);
+
+    for (const key in doc) {
+      const res1 = await chain.call({
+        input: ` here are some previous formatting for section:${key} [${doc[key].pageContent}]`,
+      });
+    }
   });
 
   // console.log(memory.chatHistory);
@@ -184,52 +217,21 @@ AI:`);
   //   .then(async () => {
   //   return await chain.call({
   const res3 = await chain.call({
-    //       input: `
-    //     You are a helpful assistant. Help the user write a Statement of Work for a consulting project.   Make sure headers are wrapped in "**" and give as much as possible
-
-    //     organization": "Ernst & Young LLP ",
-    //   "client": "Villlage of Westmont, IL",
-    //   "confidentiality": "You will not and not permit others to quote or refer to any reports to EY or any other EY firm documents related to purchase or sale of securities, or periodic reporting obligations under Securities Laws. ",
-    //   "projectScope": "EY will provide the following Services: Analysis of TIF assistance requests  by participating in project iniation meetings with representatives of the village, assist the village to identify key data, discuss TIF assistance requests the village has received or is considering, understand and confirm the villages financial expectations with respect to public benefits, assist the village in their review of TIF assistance requests by reviewing a number of agreed upon elements and benchmarks determined by the village. In additional, we will provide observations, a detailed analysis and model the pro form and gap analysis as well as prepare for client's review and approval. Finally we will prepare a summary memorandum, and assist the village in a supporting role. ",
-    //   "projectSolution": "We will assist the village with preparing the annual TIF report(s) based on policies, criteria and data provided by the village. EY will prepare an addendum to the scope of work and proposal for the annual TIF reports. ",
-    //   "payment": "You shall pay fees based on the time our professionals spend performing them, billed at the following agreed upon rates per hour: Partner/Principal, $400 - Executive Director, $400 - Senior Manager, $325 - Manager, $275 - Senior, $230 - Staff, $190. ",
-    //   "additonalInfo": "",
-    //   "Contacts": "We will communicate with Spencer Parker. Your contact at EY will be Jennifer Tammen.
-    // `,
     input: filledTemplate,
   });
-  console.log(res3.text);
-  console.log(res3.text.length);
-  console.log("END---");
-  return res3.text;
-  // awaitconsole.log("END");
-  //   });
-
-  //   console.log(res3);
+  //   console.log(res3.text);
   //   console.log(res3.text.length);
-  // console.log({ res });
-  //   const res2 = await convochain.call({
-  //     input: `what was my first message}`,
-  //   });
-  //   console.log({ res2 });
-  //   console.log();
-  // memory. note token limit is by min request we can try .sleep
-  //   const lastchain = new LLMChain({
-  //     llm: model,
-  //     memory: memory,
-  //     prompt,
-  //   });
-  //   console.log("lLAST RUN=--");
-  //   const result = await lastchain.call({
-  //     question: "what was my first message?",
-  //   });
-  //   console.log(result);
-  // });
-  //   prompt: prompt,)
+  //   console.log("END---");
+  //RESET collection
+  milClient.dropCollection({ collection_name: "sowDataCollection" });
+
+  return res3.text;
+
+  //-----------------------------------
 }
 
 // const final = await callModelforResult(
-//   `You are a helpful assistant. Help the user write a Statement of Work for a consulting project.   Make sure headers are wrapped in "**" and give as much as possible
+//   `You are a helpful assistant. Help the user write a 2 page Statement of Work for a consulting project.   Make sure headers are wrapped in "**" and give as much as possible
 
 //         organization": "Ernst & Young LLP ",
 //       "client": "Villlage of Westmont, IL",
